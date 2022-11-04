1d2b9a5d2f387008c2b5571c4846f32af8b699df
Hp <annaboiko377@gmail.com>
2022-11-04 23:46:55 +0300

    !

--- ДЗ 01.11.2022 Файлы часть 2/ДЗ 01.11.2022 Файлы часть 2.cpp
+++ ДЗ 01.11.2022 Файлы часть 2/ДЗ 01.11.2022 Файлы часть 2.cpp
@@ -19,14 +19,24 @@ int main()
     intptr_t hSearch;                                               // handle of search - дескриптор поискового ресурса
 
     const size_t SIZE = 20;
-    char pattern[SIZE];                                             //./files/*file*   
-    char term[SIZE];    
+    char pattern[SIZE];                                                
+    char folder[SIZE];
+    char term[SIZE] = { '*' };
+    char word[SIZE];
+
+    int fl = 0;       // переменная, которая считает кол-во совпадений среди названий файлов
+    int dr = 0;       // переменная, которая считает кол-во совпадений среди названий папок
 
     std::cout << " Enter folder path (for example, ./files/):\n";
     std::cin >> pattern;                                            // ./files/   - в папке files
     std::cout << " Enter a search term(for example, *file*):\n";
-    std::cin >> term;                                               // *file*      - в имени есть file в любом месте
+    std::cin >> word; 
 
+    strcat_s(term, word);                                          // выполняем конкатенацию, чтоб добавить * в слово для поиска 
+    strcat_s(term, "*");                                           // *file*     - в имени есть file в любом месте
+    std::cout << term;
+
+    strcpy_s(folder, pattern);                                      // копируем путь в folder для дальнейшей работы
     strcat_s(pattern, term);                                        // соединяем путь и имя
     //std::cout << pattern;
     
@@ -42,37 +52,43 @@ int main()
     {
         std::cout << file_info.name << "\n";
 
-        
 
+        char str[SIZE];
+        char str1[260];
+        strcpy_s(str1, folder);
+        strcat_s(str1, file_info.name);
+        //std::cout << str1 << "\n";
 
-
-
-        search_status = _findnext(hSearch, &file_info);             // вызываем ф-ю _findnext (продолжение поиска)
-        if ((file_info.attrib & _A_SUBDIR) == 0) // если найден файл, то копаем дальше по файлам
+        if ((file_info.attrib & _A_SUBDIR) != _A_SUBDIR)  // если найденный элемент является файлом, то мы его читаем
         {
-            std::ifstream reader;
-            const std::string filename = file_info.name;
-            std::cout << filename << "\n";
-
-            reader.open(filename);
-            if (!reader)  // открылся ли файл
+            fl++; // увеличиваем для названий файлов
+            std::ifstream reader(str1);        // сокращенная форма - сразу open(...)
+            if (!reader)                           // проверка нужна и для сокр формы
             {
-                std::cout << "Reader open error" << std::endl;
-                return -2;
+                std::cout << "Reader open error";
+                return -1;
             }
-
-            std::string str;
-
-            while (!reader.eof())  // пока не конец файла
+            while (!reader.eof())                         // пока не конец файла
             {
-                //reader >> str;
-                std::getline(reader, str);
-                std::cout << str << "\n";
+                reader.getline(str, SIZE);             // считываем строку из файла в str
+                std::cout << str;
             }
+            std::cout << std::endl;
+
             reader.close();
         }
+        else
+        {
+            dr++; // увеличиваем для названий папок
+        }
+
+        search_status = _findnext(hSearch, &file_info);             // вызываем ф-ю _findnext (продолжение поиска)
+        
 
     } while (search_status == 0);                                   // возвращает успешную операцию
+
+    std::cout << " Count of file name " << fl << "\n"
+        << " Count of folder name " << dr << "\n";
 
     _findclose(hSearch);
 
